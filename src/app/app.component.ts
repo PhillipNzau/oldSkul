738f1f5f5fb0a2f6005fc32565aea3e6859fe3bd
phillipnzau <nzauonline@gmail.com>
2023-07-08 23:40:16 +0300

    form complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ContactService } from './contact.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'oldSkul';
+
+  form: FormGroup = new FormGroup({
+    name: new FormGroup(''),
+    number: new FormGroup(''),
+    className: new FormGroup(''),annualContribution: new FormGroup(''),
+  });
+  submitted =false;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      name:['',[Validators.required]],
+      number:[''],
+      className:[ '',[Validators.required] ],
+      annualContribution:[0,[Validators.minLength(1)]],
+    })
+  }
+
+  onSubmit() {
+    const form = this.form.value
+    console.log(form.name);
+
+    const subject = 'Toba';
+    const body = `
+      Name: ${form.name}
+      Number: ${form.number}
+      Class: ${form.className}
+      Annual Contribution: ${form.annualContribution}
+    `;
+    const mailtoUrl = `mailto:nzau78@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.location.href = mailtoUrl;
+  }
 }
